07c9a4a189dcc51878c38583702e6bbbcf232950
Parth Gupta <parth.eng1210@gmail.com>
2024-07-08 17:54:05 +0530

    feat: implement ThetaPlayer component

---
+++ src/components/thetaPlayer.tsx
@@ -0,0 +1,80 @@
+"use client"
+import { useTurnkeyContext } from "@/context/turnkeyContext";
+import { LegacyRef, useEffect, useRef } from "react";
+
+let TVA_JS_VERSION_NUMBER = '1.0.12';
+
+function getSignTypedDataJson(timestamp: number) {
+    return {
+        domain: {
+            name: 'Theta DRM',
+            version: '1',
+            chainId: 365 
+        },
+        message: {
+            contents: "Theta DRM Sign In",
+            timestamp: timestamp
+        },
+        primaryType: "SignIn",
+        types: {
+            EIP712Domain: [{
+            name: "name",
+            type: "string"
+            }, {
+            name: "version",
+            type: "string"
+            }, {
+            name: "chainId",
+            type: "uint256"
+            }],
+            SignIn: [{
+            name: "timestamp",
+            type: "uint256"
+            }, {
+            name: "contents",
+            type: "string"
+            }]
+        }
+    };
+  };
+
+const ThetaPlayer: React.FC = () => {
+    const playerRef = useRef<HTMLVideoElement>(null)
+    const {signer} = useTurnkeyContext()
+    useEffect(() => {
+        if (signer) {
+            console.log(signer.getAddress().then(alert))
+            const timestamp = Date.now();
+            const data = getSignTypedDataJson(timestamp);
+            (async () => {
+                const signature = await signer.signTypedData(data.domain, data.types, data.message)
+                console.log(signature)
+                let script = document.createElement('script');
+                script.src = "https://assets.thetatoken.org/tva-js/" + TVA_JS_VERSION_NUMBER + "/tva.js";
+                script.async = true;
+                const address = await signer.getAddress();
+                const a = { address, timestamp, signature }
+                script.onload = function () {
+                    // setTvaLibLoaded(true);
+                    new window.TVA.Video({
+                    videoId: "video_5ywtntfnhffd7ndvj1auw8ij2t",
+                    server: 'tva',
+                    videoEl: playerRef.current,
+                    networkId: 365,
+                    onError: function (error: any) {console.log(error)},
+                    signin: a
+                    });
+                };
+                document.body.appendChild(script);
+                
+            })()
+        }
+    })
+    return (
+        <div>
+            <video ref={playerRef} controls/>
+        </div>
+    )
+}
+
+export default ThetaPlayer
---
+++ src/pages/watch/[id].tsx
@@ -0,0 +1,12 @@
+import ThetaPlayer from '@/components/thetaPlayer';
+
+const WatchPage: React.FC = () => {
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 className="text-4xl font-bold">Watch Page</h1>
+            <ThetaPlayer/>
+        </div>
+    );
+};
+
+export default WatchPage;
