a797124ae32edc6d419713342869ca8633266fcd
Parth Gupta <parth.eng1210@gmail.com>
2024-07-09 20:51:24 +0530

    refactor: rename test.ts to multiAddress.ts

--- src/pages/api/multiAddress.ts
+++ src/pages/api/multiAddress.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     try {
-        const response = await fetch('http://13.200.213.161:3000/multiaddress'); // Replace with the URL you want to fetch from
+        const response = await fetch('http://13.200.213.161:3000/multiaddress');
         const data = await response.json();
 
         res.status(200).json(data);
