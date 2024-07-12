c54625073283fe81ec077557cc01ee3e56e02ca6
Parth Gupta <parth.eng1210@gmail.com>
2024-07-12 15:53:21 +0530

    chore: add p2p.thetre.live host

--- next.config.js
+++ next.config.js
@@ -8,6 +8,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/**',
             },
+            {
+                protocol: 'https',
+                hostname: 'p2p.thetre.live',
+                port: '',
+                pathname: '/**',
+            },
         ],
     },
 };
