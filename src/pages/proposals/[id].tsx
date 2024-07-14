01b0dba0a88a7cf20903fd3d85a8df247ab50ca8
Parth Gupta <parth.eng1210@gmail.com>
2024-07-14 13:19:17 +0530

    style: updated proposal details page layout

--- src/pages/proposals/[id].tsx
+++ src/pages/proposals/[id].tsx
@@ -36,7 +36,6 @@ const Proposal: React.FC = () => {
     return (
         <div>
 
-            <p className='text-center text-white font-bold text-2xl'>Movie Name - {proposal.data.title}</p>
             <div className="w-full h-full grid gap-4 p-4" style={{
                 gridTemplateColumns: "1fr repeat(6, minmax(auto, 10em)) 1fr",
                 gridTemplateRows: "minmax(1em, auto) 1fr auto minmax(1em, auto)"
@@ -44,15 +43,15 @@ const Proposal: React.FC = () => {
                 <div className='w-full h-full col-start-0 col-span-5'>
                     <ThetaPlayer videoId={"video_mg3tvfr4hzutanrfrru714kw0u"} type="FREE" styles="w-full h-96" />
                 </div>
-                <div className='col-start-6 col-span-3 flex flex-col gap-16'>
+                <div className='col-start-6 col-span-3 flex flex-col gap-16 justify-center'>
                     <div className='flex flex-col gap-2'>
+                        <p className='text-white font-bold text-2xl'>Movie Name - {proposal.data.title}</p>
                         <p className="font-bold text-white">{proposal.data.description}</p>
                         <p className="font-bold text-white">Genre: {proposal.data.genre}</p>
                         <p className="font-bold text-white">Cast: {proposal.data.cast}</p>
                         <p className="font-bold text-white">Directed By: {proposal.data.director}</p>
                         <p className="font-bold text-white">Produced By: {proposal.data.producer}</p>
                         <p className="font-bold text-white">Available On: {proposal.data.platforms}</p>
-
                     </div>
                     <div className="flex space-x-4">
                         <button className="bg-green-500 text-white font-bold py-2 px-4 rounded-lg hover:bg-green-700">
