3df7472ce5a7735924c7beb6df2f84c0c7183c73
Parth Gupta <parth.eng1210@gmail.com>
2024-07-14 14:26:26 +0530

    chore: updated governer abi

--- src/utils/abis/governerABI.ts
+++ src/utils/abis/governerABI.ts
@@ -113,4 +113,52 @@ export const governerABI = [
         "stateMutability": "view",
         "type": "function"
       },
+      {
+        "inputs": [
+            {
+                "internalType": "uint256",
+                "name": "proposalId",
+                "type": "uint256"
+            },
+            {
+                "internalType": "uint8",
+                "name": "support",
+                "type": "uint8"
+            }
+        ],
+        "name": "castVote",
+        "outputs": [
+            {
+                "internalType": "uint256",
+                "name": "",
+                "type": "uint256"
+            }
+        ],
+        "stateMutability": "nonpayable",
+        "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "account",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "timepoint",
+          "type": "uint256"
+        }
+      ],
+      "name": "getVotes",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
 ]
