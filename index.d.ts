7e075a3c1d112ad3b7a738006f7a1c3da61601c5
Parth Gupta <parth.eng1210@gmail.com>
2024-07-16 17:14:05 +0530

    style: added missing semicolon

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 declare module '@thetalabs/theta-js';
 declare module 'react-slick';
-declare module 'uint8arrays'
+declare module 'uint8arrays';
 interface Window {
     TVA: any;
     libp2p: any;
