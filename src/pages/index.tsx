3d3a64520255b0b853dcfb3f5b9651b1b6381f55
Parth Gupta <parth.eng1210@gmail.com>
2024-07-17 21:45:10 +0530

    chore: removed dummy data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,57 +1,25 @@
 
+import Loader from "@/components/loader";
 import MovieCard from "@/components/movieCard";
 import MovieSlider from "@/components/movieSlider";
+import { ProposalState, useThetreContext } from "@/context/thetreContext";
 import { useEffect } from "react";
 
 export default function Home() {
-  // useEffect(() => {
-  //   let lastScrollY = window.scrollY;
-
-  //   const smoothScroll = (targetY: number, duration: number) => {
-  //     const startY = window.scrollY;
-  //     const diffY = targetY - startY;
-  //     let start: number;
+  const { proposalDetails, fetchProposals, setLoader, loading } = useThetreContext()
+  useEffect(() => {
+    (async () => {
+      await setLoader(fetchProposals)
+    })()
+  }, [])
   
-  //     const step = (timestamp: number) => {
-  //       if (!start) start = timestamp;
-  //       const time = timestamp - start;
-  //       const percent = Math.min(time / duration, 1);
-  //       window.scrollTo(0, startY + diffY * percent);
-  //       if (time < duration) {
-  //         requestAnimationFrame(step);
-  //       }
-  //     };
-  
-  //     requestAnimationFrame(step);
-  //   };
-
-  //   const handleScroll = () => {
-  //     const currentScrollY = window.scrollY;
-  //     const sliderOffsetTop = document.getElementById("slider")!.offsetTop;
-  //     const moviesOffsetTop = document.getElementById("movies")!.offsetTop;
-
-  //     if (currentScrollY > lastScrollY && currentScrollY < moviesOffsetTop) {
-  //       // Scroll down
-  //       smoothScroll(moviesOffsetTop, 50);
-  //     } else if (currentScrollY < lastScrollY && currentScrollY > sliderOffsetTop) {
-  //       // Scroll up
-  //       smoothScroll(sliderOffsetTop, 50);
-  //     }
-
-  //     lastScrollY = currentScrollY;
-  //   };
-
-  //   window.addEventListener("scroll", handleScroll);
-
-  //   return () => {
-  //     window.removeEventListener("scroll", handleScroll);
-  //   };
-  // }, []);
-
+  if (loading) {
+    return <Loader />
+  }
   return (
     <div>
       <main className="h-screen" id="slider">
-        <MovieSlider />
+        <MovieSlider proposalDetails={proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState)))}/>
       </main>
       <div className="flex h-screen" id="movies">
         <div>
