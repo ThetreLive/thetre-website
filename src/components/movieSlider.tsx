8a9a345793994f7ec6835e9bf98d134c0b8fd093
Parth Gupta <parth.eng1210@gmail.com>
2024-07-18 19:08:47 +0530

    style: fixed movie slider styles

--- src/components/movieSlider.tsx
+++ src/components/movieSlider.tsx
@@ -54,25 +54,25 @@ const MovieSlider = (props: { proposalDetails: ProposalDetails[], access: string
       boxShadow: 'inset 65px 41px 115px 0px rgba(0,0,0,0.75)',
     }}>
       <Slider {...settings}>
-        {props.proposalDetails.map((movie, index) => (
+        {props.proposalDetails.slice(0, 3).map((movie, index) => (
           <div key={index} className='relative h-full w-full'>
             <Image src={getFileURL(JSON.parse(movie.data.coverLink as string).result.key, JSON.parse(movie.data.coverLink as string).result.relpath)} alt={movie.data.title} className="absolute inset-0 w-full h-full object-cover" fill/>
             <div className="absolute inset-0"></div>
             <div className="relative z-10 flex lg:items-center items-end h-full px-16">
               <div className="text-left text-white lg:w-1/3 bg-black bg-opacity-50 lg:p-8 p-4 rounded-lg backdrop-blur-xl">
-                <h1 className="lg:text-5xl text-3xl font-bold mb-4">{movie.data.title}</h1>
-                <p className="lg:text-lg text-sm mb-4">{movie.data.genre}</p>
+                <h1 className="lg:text-4xl text-2xl font-bold mb-4">{movie.data.title}</h1>
+                <p className="lg:text-base text-sm mb-4">{movie.data.genre}</p>
                 {/* <p className="text-sm mb-8">Rating: {movie.rating}</p> */}
-                <p className="mb-8 lg:text-base text-sm">{movie.data.description}</p>
+                <p className="mb-8 text-sm">{movie.data.description}</p>
                 <div className="flex space-x-4">
                   {props.access.includes(movie.data.title) || !movie.data.isDRMEnabled ? (
-                    <Link href={`/watch/${movie.id}`} className="bg-custom-radial px-6 py-3 font-bold rounded-full">Watch Now</Link>
+                    <Link href={`/watch/${movie.id}`} className="bg-custom-radial px-6 py-3 font-bold rounded-xl">Watch Now</Link>
                   ) : (
-                    <button onClick={() => buyTicket(movie.data.title)} className="bg-custom-radial px-6 py-3 font-bold rounded-full">Buy Pass for 10TFUEL</button>   
+                    <button onClick={() => buyTicket(movie.data.title)} className="bg-custom-radial px-6 py-3 font-bold rounded-xl">Buy Pass for 10TFUEL</button>   
                     )}
                   
-                  <button className="bg-gray-700 px-4 py-2 rounded-full">Trailer</button>
-                  <button className="bg-gray-700 px-4 py-2 rounded-full">More</button>
+                  <button className="bg-gray-700 px-4 py-2 rounded-xl">Trailer</button>
+                  <button className="bg-gray-700 px-4 py-2 rounded-xl">More</button>
                 </div>
               </div>
             </div>
