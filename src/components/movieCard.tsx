27da7e707169f1b7255ea5df07ed842ecba85be8
Parth Gupta <parth.eng1210@gmail.com>
2024-07-20 16:36:30 +0530

    chore: removed dummy data from moviecard

--- src/components/movieCard.tsx
+++ src/components/movieCard.tsx
@@ -1,20 +1,41 @@
+import { ProposalDetails, useThetreContext } from '@/context/thetreContext';
+import { getFileURL } from '@/utils/theta';
+import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
-const MovieCard: React.FC = () => {
+interface Props {
+    proposal: ProposalDetails,
+    access: string[]
+}
+
+const MovieCard: React.FC<Props> = ({proposal, access}) => {
+    const {buyTicket} = useThetreContext();
     return (
-        <div className="bg-white rounded-lg shadow-lg p-4">
-            <img
-                className="w-full h-48 object-cover rounded-t-lg"
-                src="https://example.com/movie-poster.jpg"
-                alt="Movie Poster"
-            />
+        <div className="bg-bg-blue border border-gray-400/40 text-white rounded-lg shadow-lg p-4 flex-1">
+            <div className='w-full h-48 relative'>
+                <Image
+                    className="rounded-t-lg"
+                    src={getFileURL(JSON.parse(proposal.data.coverLink as string).result.key, JSON.parse(proposal.data.coverLink as string).result.relpath)} 
+                    alt={proposal.data.title}
+                    objectFit='cover'
+                    objectPosition='top'
+                    fill
+                />
+
+            </div>
             <div className="p-4">
-                <h2 className="text-xl font-bold mb-2">Movie Title</h2>
-                <p className="text-gray-600">Movie Description</p>
-                <div className="flex justify-between items-center mt-4">
-                    <span className="text-gray-500">Release Date: 2022-01-01</span>
-                    <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg">
-                        Watch Now
+                <h2 className="text-xl font-bold mb-2">{proposal.data.title}</h2>
+                <p className="text-gray-200">Genre - {proposal.data.genre}</p>
+                <p className="text-gray-200">{proposal.data.description.slice(0, 100)}...</p>
+                <div className="flex justify-between items-center mt-4 gap-2">
+                {access.includes(proposal.data.title) || !proposal.data.isDRMEnabled ? (
+                    <Link href={`/watch/${proposal.id}`} className="bg-custom-radial px-6 py-3 font-bold rounded-xl">Watch Now</Link>
+                  ) : (
+                    <button onClick={() => buyTicket(proposal.data.title)} className="bg-custom-radial px-6 py-3 font-bold rounded-xl">Buy Pass for 10TFUEL</button>   
+                    )}
+                    <button className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-lg flex-1">
+                        Trailer
                     </button>
                 </div>
             </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,11 +6,30 @@ import MovieSlider from "@/components/movieSlider";
 import { ProposalState, useThetreContext } from "@/context/thetreContext";
 import { useWalletContext } from "@/context/walletContext";
 import { useEffect } from "react";
+import { useState } from "react";
 import Navbar from "@/components/navbar";
 
 export default function Home() {
   const { proposalDetails, fetchProposals, setLoader, loading } = useThetreContext()
   const { access } = useWalletContext()
+  const [currentPage, setCurrentPage] = useState(1);
+  const moviesPerPage = 3;
+
+  const indexOfLastMovie = currentPage * moviesPerPage;
+  const indexOfFirstMovie = indexOfLastMovie - moviesPerPage;
+  console.log(indexOfFirstMovie, indexOfLastMovie)
+  const handleNextPage = () => {
+    if (currentPage < Math.ceil(proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState))).length / moviesPerPage)) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  
+  const handlePrevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
   useEffect(() => {
     (async () => {
       await setLoader(fetchProposals)
@@ -27,16 +46,28 @@ export default function Home() {
       <main className="h-[50vh]" id="slider">
         <MovieSlider access={access} proposalDetails={proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState)))}/>
       </main>
-      <div className="flex h-screen" id="movies">
-        <div>
-          <MovieCard />
-          <MovieCard />
+      <div className="flex flex-row flex-wrap gap-4 p-4 w-full justify-center" id="movies">
+          {proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState))).slice(indexOfFirstMovie, indexOfLastMovie).map((proposal, index) => (
+            <MovieCard key={index} proposal={proposal} access={access}/>
+          ))}
         </div>
-        <div>
-          <MovieCard />
-          <MovieCard />
+        <div className="flex justify-center items-center space-x-4 p-4">
+          <button
+            className={`px-4 py-2 text-white ${currentPage === 1 ? 'bg-gray-400' : 'bg-blue-600'} rounded`}
+            onClick={handlePrevPage}
+            disabled={currentPage === 1}
+          >
+            Previous
+          </button>
+          <span className="text-white">{`Page ${currentPage} of ${Math.ceil(proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState))).length / moviesPerPage)}`}</span>
+          <button
+            className={`px-4 py-2 text-white ${currentPage === Math.ceil(proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState))).length / moviesPerPage) ? 'bg-gray-400' : 'bg-blue-600'} rounded`}
+            onClick={handleNextPage}
+            disabled={currentPage === Math.ceil(proposalDetails.filter(proposal => [ProposalState.Succeeded, ProposalState.Executed, ProposalState.Queued].includes(Number(proposal.proposalState))).length / moviesPerPage)}
+          >
+            Next
+          </button>
         </div>
-      </div>
       </div>
 
     </div>
