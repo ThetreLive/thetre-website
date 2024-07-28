670ecb68379e25de91c4fd23d047dbd915c65f4f
Parth Gupta <parth.eng1210@gmail.com>
2024-07-28 13:39:06 +0530

    chore: added origin check

--- src/pages/api/s3ApiGateway.ts
+++ src/pages/api/s3ApiGateway.ts
@@ -1,15 +1,16 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import formidable,{ Fields, Files } from 'formidable';
+import formidable, { Fields, Files } from 'formidable';
 import FormData from 'form-data';
 import fs from 'fs';
 import fetch from 'node-fetch';
-
 
 export const config = {
   api: {
     bodyParser: false,
   },
 };
+
+const allowedOrigin = process.env.ALLOWED_ORIGIN;
 
 const uploadFileToApiGateway = async (file: any, filename: string): Promise<void> => {
   const url = `https://65utciwfek.execute-api.ap-south-1.amazonaws.com/dev/thetre/${filename}`;
@@ -25,13 +26,19 @@ const uploadFileToApiGateway = async (file: any, filename: string): Promise<void
     },
     body: fs.createReadStream(file.filepath),
   });
-  console.log(response)
 
   console.log('File uploaded successfully to S3');
 };
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method === 'POST') {
+    const referer = req.headers.referer;
+    
+    if (!referer || !referer.startsWith(allowedOrigin!)) {
+      res.status(403).json({ error: 'Forbidden' });
+      return;
+    }
+
     const form = formidable({});
 
     form.parse(req, async (err: any, fields: Fields, files: Files) => {
@@ -43,18 +50,17 @@ const handler = async (req: NextApiRequest, res: NextApiResponse) => {
       const file = (files.file as formidable.File[])[0] as formidable.File;
       const filename = (fields.filename as string[])[0] as string;
 
-    if (!filename) {
-      res.status(400).json({ error: 'Filename is required' });
-      return;
-    }
+      if (!filename) {
+        res.status(400).json({ error: 'Filename is required' });
+        return;
+      }
 
-    try {
-      await uploadFileToApiGateway(file, filename);
-      console.log("hee")
-      res.status(200).json({ message: 'File uploaded successfully' });
-    } catch (error) {
-      console.log(error)
-      res.status(500).json({ error: 'Error uploading file' });
+      try {
+        await uploadFileToApiGateway(file, filename);
+        res.status(200).json({ message: 'File uploaded successfully' });
+      } catch (error) {
+        console.log(error);
+        res.status(500).json({ error: 'Error uploading file' });
       }
     });
   } else {
